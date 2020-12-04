05be111e56f6ddfdd91e2a37613250202cf6b3e1
jholmes16 <jholmesarea@gmail.com>
2020-12-04 09:33:48 -0500

    Add alert messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
   def new
   end
 
@@ -6,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:success] = "You have successfully logged in"
+      flash[:success] = "You have successfully logged in" #key to message_type in messages partial
       redirect_to root_path
     else
       flash.now[:error] = "There was something wrong with your login information"
@@ -18,5 +19,14 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = "You have successfully logged out"
     redirect_to login_path
+  end
+
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
   end
 end
